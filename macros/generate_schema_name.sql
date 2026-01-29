022c758df34c6a6f72732ac5d46415cdb4b2a2cd
mancer2026v1@gmail.com <mancer2026v1@gmail.com>
2026-01-29 12:35:18 +0000

    .

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,14 +1,8 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-
+    -- check if the target schema is different to dev to use it
+    {%- if not target.name == 'dev' and custom_schema_name is not none -%}
         {{ custom_schema_name | trim }}
-
+    {%- else -%}
+        {{ target.schema }}
     {%- endif -%}
-
 {%- endmacro %}
